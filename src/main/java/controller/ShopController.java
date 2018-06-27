bafbea81a7e6235488b0372ece1977261bc2d686
user <user@DESKTOP-MFSOE8E>
2018-06-27 21:38:50 +0900

    김민수 : commit

--- src/main/java/controller/ShopController.java
+++ src/main/java/controller/ShopController.java
@@ -1,22 +1,50 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import exception.ShopException;
 import logic.Shop;
 import logic.ShopService;
 
 @Controller
 public class ShopController {
 	@Autowired
-	private ShopService shopservice;
+	private ShopService shopService;
 	
-	@RequestMapping("shop/*")
-	public ModelAndView add() {
+	@RequestMapping(value="shop/write", method=RequestMethod.GET)
+	public ModelAndView write() {
+		System.out.println("게시물 작성 GET 방식 호출입니다.");
 		ModelAndView mav = new ModelAndView();
 		mav.addObject(new Shop());
+		return mav;
+	}
+	
+	@RequestMapping(value="shop/write", method=RequestMethod.POST)
+	public ModelAndView write(@Valid Shop shop, BindingResult bindingResult, HttpServletRequest request) {
+		System.out.println("게시물 작성 POST 방식 호출입니다.");
+		ModelAndView mav = new ModelAndView();
+		
+		if(bindingResult.hasErrors()) {
+			System.out.println("게시물 작성 검증 오류입니다.");
+			mav.getModel().putAll(bindingResult.getModel());
+			return mav;
+		}
+		try { 
+			shopService.shopWrite(shop, request);
+			mav.setViewName("redirect:list.shop");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			new ShopException("게시물 등록 실패", "write.zips");
+		}
 		return mav;
 	}
 }
--- src/main/java/dao/ShopDaoImpl.java
+++ src/main/java/dao/ShopDaoImpl.java
@@ -4,9 +4,22 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import dao.mapper.ShopMapper;
+import logic.Shop;
+
 @Repository
 public class ShopDaoImpl implements ShopDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	private final String NS = "dao.mapper.ShopMapper.";
+	
+	// 게시물 작성
+	@Override
+	public void shopWrite(Shop shop) {
+		int shop_no = sqlSession.getMapper(ShopMapper.class).maxNo();
+		shop.setShop_no(++shop_no);			
+		sqlSession.getMapper(ShopMapper.class).insert(shop);
+	}
+	
+	
 }
