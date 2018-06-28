0c4387762f75f97ee218ee82993e095705da4acd
user <user@DESKTOP-4M1T94F>
2018-06-28 18:28:46 +0900

    주한 : 2018.06.28 게시판

--- src/main/java/dao/mapper/BoardMapper.java
+++ src/main/java/dao/mapper/BoardMapper.java
@@ -1,42 +1,18 @@
 package dao.mapper;
 
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import logic.Board;
 
 public interface BoardMapper {
 
-	@Insert("insert into board (name,pass,subject,content,file1,regdate,readcnt,ref,reflevel,refstep)" + 
-			"values(#{name},#{pass},#{subject},#{content},#{fileurl},now(),0,#{ref},#{reflevel},#{refstep})")
+	@Insert("insert into totalboard (board_userid,subject,content,filename,regdate,board_count,board_type)" + 
+			"values(#{board_userid},#{subject},#{content},#{fileurl},now(),0,#{board_type})")
 	int insert(Board board);
 
-	@Update("update board set readcnt=readcnt+1 where num=#{value}")	
+	@Update("update totalboard set board_count=board_count+1 where num=#{value}")	
 	void readcnt(Integer num);
-
-	@Select("select ifnull(max(num),0) from board")
-	int maxNum();
-
-	@Update("update board set refstep = refstep + 1" + 
-			" where ref = #{ref} and refstep > #{refstep}")
-	void refstepadd(Board board);
-
-	@Update("update board set name=#{name},subject=#{subject},content=#{content},regdate=now(),file1=#{fileurl} where num=#{num}")
-	int update(Board board);
-	
-	@Delete("delete from board where num=#{num}")
-	int delete(Board board);
-
-	@Delete("delete from board where num=#{num}")
-	int mapdelete(Integer integer);
-
-	@Select("select name key1, count(*) value1 from board group by name having count(*) > 0")
-	List<Map<String, Object>> graph();
 
 
 }
--- src/main/java/logic/BoardService.java
+++ src/main/java/logic/BoardService.java
@@ -1,6 +1,20 @@
 package logic;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 public interface BoardService {
+
+	int boardcount(String searchType, String searchContent);
+
+	List<Board> boardList(String searchType, String searchContent, Integer pageNum, int limit);
+
+	int boardinsert(Board board, HttpServletRequest request);
+
+	Board getBoard(Integer num);
+
+	void updatereadcnt(Integer num);
 
 	
 }
